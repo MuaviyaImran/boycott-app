fea89b3c3103101e0b23a74be9c470e16495d1ea
muaviyaimran <muaviyaimran1122@gmail.com>
2023-11-24 16:19:08 +0500

    env error for deploy fixed

--- next.config.js
+++ next.config.js
@@ -1,15 +1,12 @@
 /** @type {import('next').NextConfig} */
 const dotenv = require("dotenv");
+
 dotenv.config();
 const nextConfig = {
   output: "export",
   distDir: "dist",
   reactStrictMode: true,
-  env: {
-    MONGO_URL: process.env.MONGO_URL,
-    ADMIN_EMAIL: process.env.ADMIN_EMAIL,
-    AUTH_SECRET: process.env.AUTH_SECRET,
-  },
+
   experimental: {
     serverComponentsExternalPackages: ["@acme/ui"],
   },
