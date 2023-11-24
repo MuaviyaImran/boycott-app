a3d8db61b30572fd1ebe428a87adc9857c372722
muaviyaimran <muaviyaimran1122@gmail.com>
2023-11-24 18:49:12 +0500

    updated baseURL

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Layout from "templates/templates";
 import Homepage from "./home";
-import Test from "components/suggestionModal";
 export default function Index() {
+  if (!process.env.NEXT_PUBLIC_BASE_URL) return null;
   return (
     <Layout>
       <Homepage />
