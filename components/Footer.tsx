9352554793007237cecb632fca33b019d49f322d
muaviyaimran <muaviyaimran1122@gmail.com>
2023-11-26 04:05:27 +0500

    Added Navbar Height

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Image from "next/image";
 const Footer: React.FC = () => {
   return (
-    <div className="bg-primary-backgroundC z-10 sticky bottom-0 w-full p-1 text-primary-fontC">
+    <div className="bg-primary-backgroundC z-10 sticky bottom-0 w-full p-1 text-primary-fontC h-[60px]">
       <div className="lg:max-width flex gap-3 justify-between items-center py-1 px-3 flex-col md:flex-row text-sm md:text-base">
         <p className="flex items-center justify-center gap-1">
           <span className="text-[2rem] mt-3">&copy;</span> 2023 Boycott Israel.
